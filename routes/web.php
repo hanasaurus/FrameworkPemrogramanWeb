a7668bf17966a45f1683e5eecc6e8b096ba4d287
hanasaurus <hannafadillah000@gmail.com>
2026-09-02 13:36:35 +0700

    praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
 
+Route::get('/about', function () {
+    return 'Toko POS merupakan aplikasi yang digunakan untuk membantu proses penjualan, pengelolaan produk, dan transaksi toko.';
+});
