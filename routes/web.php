8cb761f4efb99f75e0d2326e1e9a380012a6ee2a
hanasaurus <hannafadillah000@gmail.com>
2026-09-02 13:02:43 +0700

    praktikum 2

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
+
